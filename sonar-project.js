deb586d81ef5c8b740c923eecaec499109bb171f
vinod264 <102505969+vinod264@users.noreply.github.com>
2022-05-02 19:48:07 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.235.83.135:9000/',
+  serverUrl: 'http://13.235.83.135:9000',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
